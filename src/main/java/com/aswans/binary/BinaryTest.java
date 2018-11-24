30745e3f084e436dfd924eb8ecad9649541df377
aswans <javaerman@163.com>
2018-11-24 14:44:44 +0800

    增加java对二进制、八进制、十进制、十六进制的操作

---
+++ src/main/java/com/aswans/binary/BinaryTest.java
@@ -0,0 +1,11 @@
+package com.aswans.binary;
+
+public class BinaryTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          byte a = 012;
+          System.out.println(a);
+	}
+
+}
